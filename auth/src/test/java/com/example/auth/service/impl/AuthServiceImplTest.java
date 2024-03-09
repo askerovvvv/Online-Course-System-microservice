6cf6a8b04506aa5a7fc5eee13b5e05eb9f152944
Bekbol <askerov.bekbol@inbox.ru>
2024-03-09 17:02:19 +0600

    authenticate test

--- auth/src/test/java/com/example/auth/service/impl/AuthServiceImplTest.java
+++ auth/src/test/java/com/example/auth/service/impl/AuthServiceImplTest.java
@@ -1,14 +1,17 @@
 package com.example.auth.service.impl;
 
 import com.example.auth.config.JwtService;
+import com.example.auth.models.dto.AuthenticationRequest;
+import com.example.auth.models.dto.AuthenticationResponse;
 import com.example.auth.models.dto.RegisterRequest;
 import com.example.auth.models.entity.EmailVerificationToken;
+import com.example.auth.models.entity.Role;
 import com.example.auth.models.entity.User;
 import com.example.auth.repository.EmailVerificationRepository;
-import com.example.auth.repository.RoleRepository;
 import com.example.auth.repository.UserRepository;
 import com.example.auth.service.EmailSender;
 import com.example.auth.service.EmailVerificationService;
+import com.example.auth.service.RoleService;
 import com.example.auth.validator.AuthValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,6 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -30,7 +34,7 @@ class AuthServiceImplTest {
     @Mock
     private UserRepository userRepository;
     @Mock
-    private RoleRepository roleRepository;
+    private RoleService roleService;
     @Mock
     private EmailVerificationService verificationService;
     @Mock
@@ -50,31 +54,37 @@ class AuthServiceImplTest {
     public void setUp() {
         MockitoAnnotations.openMocks(this);
         mockAuthService = new AuthServiceImpl(
-                verificationRepository,
                 userRepository,
-                roleRepository,
+                verificationRepository,
+                roleService,
                 verificationService,
                 jwtService,
-                authenticationManager,
-                passwordEncoder,
+                emailSender,
                 registerValidator,
-                emailSender);
+                passwordEncoder,
+                authenticationManager
+        );
     }
 
     @Test
     void testRegister_Success() {
         // given
+
         RegisterRequest registerData = new RegisterRequest();
         registerData.setFirstname("test name");
         registerData.setEmail("test@example.com");
+
+        Role mockRole = new Role();
 
         User mockUser = new User();
         mockUser.setFirstname("test name");
         mockUser.setEmail("test@example.com");
         mockUser.setEmailVerified(false);
+        mockUser.setRole(new HashSet<>(List.of(mockRole)));
         EmailVerificationToken mockVerificationToken = new EmailVerificationToken();
 
         // Mock actions
+        when(roleService.getRoleByName(anyString())).thenReturn(mockRole);
         when(userRepository.save(any(User.class))).thenReturn(mockUser);
         when(verificationService.createToken(any(User.class))).thenReturn(mockVerificationToken);
         when(emailSender.buildEmail(anyString(), anyString())).thenReturn("someValue");
@@ -86,6 +96,28 @@ class AuthServiceImplTest {
         verify(userRepository, times(1)).save(mockUser);
         verify(emailSender).send(eq("test@example.com"), anyString());
         assertEquals("Registered", actualResponse);
-
     }
+
+    @Test
+    void testAuthenticate_Success() {
+        // given
+        AuthenticationRequest authenticateData = new AuthenticationRequest();
+        authenticateData.setEmail("test@example.com");
+        authenticateData.setPassword("password");
+
+        User mockUser = new User();
+        mockUser.setFirstname("test name");
+
+        // mock actions
+        when(authenticationManager.authenticate(any())).thenReturn(null);
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(mockUser));
+        when(jwtService.generateToken(mockUser)).thenReturn("Generated token");
+
+        // when
+        AuthenticationResponse actualResponse = mockAuthService.authenticate(authenticateData);
+
+        // then
+        assertEquals("Generated token", actualResponse.getToken());
+    }
+
 }
