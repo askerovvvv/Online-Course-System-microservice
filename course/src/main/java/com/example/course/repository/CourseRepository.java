655f1381ecaaecfb8f7cef1d5a3bbf8fe6de739a
Bekbol <askerov.bekbol@inbox.ru>
2024-03-12 15:57:03 +0600

    repositories

---
+++ course/src/main/java/com/example/course/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.example.course.repository;
+
+import com.example.course.models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    boolean findByTeacherId(Long id);
+
+}
