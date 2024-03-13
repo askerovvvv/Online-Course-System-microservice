a3a06cfcf29f9e505a9aa827c78e506dac80d709
Bekbol <askerov.bekbol@inbox.ru>
2024-03-13 14:31:09 +0600

    course controller

---
+++ course/src/main/java/com/example/course/controller/CourseController.java
@@ -0,0 +1,38 @@
+package com.example.course.controller;
+
+import com.example.course.models.dto.CourseDto;
+import com.example.course.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/course")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @PostMapping("/add/course")
+    public ResponseEntity<?> addCourse(@RequestBody CourseDto courseData) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.addCourse(courseData));
+    }
+
+    @GetMapping("/get/courses")
+    public ResponseEntity<?> getAllCourses() {
+        return ResponseEntity.status(HttpStatus.OK).body(courseService.getAllCourses());
+    }
+
+    @GetMapping("/get/course")
+    public ResponseEntity<?> getCourseById(@RequestParam("id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(courseService.getCourseById(id));
+    }
+
+    @DeleteMapping("/delete/course")
+    public ResponseEntity<?> deleteCourseById(@RequestParam("id") Long id) {
+        courseService.deleteCourse(id);
+        return ResponseEntity.status(200).build();
+    } // TODO: update
+
+}
